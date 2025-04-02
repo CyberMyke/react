f03b8ecde4792b1a81fc5bb7104a3e4b859c0ed5
CyberMyke <mykehirst@gmail.com>
2025-04-02 11:52:53 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+function MyButton() {
+  return <button>I'm a button</button>;
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
